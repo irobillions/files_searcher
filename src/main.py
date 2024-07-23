61374b726a0a9d6b96c708eaeaa5a2e52ba0d06b
irobilllions <christbouka14@yahoo.fr>
2024-07-23 02:32:25 -0400

    debut main

---
+++ src/main.py
@@ -0,0 +1,11 @@
+class Main:
+    def __init__(self, logs=None):
+        self.logs = logs
+
+    def run(self):
+        raise NotImplemented("not yet")
+
+
+if __name__ == "__main__":
+    app = Main()
+    app.run()
