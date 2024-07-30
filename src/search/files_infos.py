f0931e1b6ff6254679156d37800c2c230790b76a
irobilllions <christbouka14@yahoo.fr>
2024-07-30 02:05:59 -0400

    ajout du type pour considerer un fichier

--- src/search/files_infos.py
+++ src/search/files_infos.py
@@ -1,20 +1,25 @@
 class FileInfos:
-    def __init__(self, name, path, abstract=None):
+    def __init__(self, name, path, type, abstract=None):
         self.name = name
         self.path = path
+        self.type = type
         self.abstract = abstract
 
     def __str__(self):
         return f"__File Information__\n " \
                f"filename: {self.name}\n " \
                f"path: {self.path}\n " \
+               f"type: {self.type}\n " \
                f"resume: {self.abstract}"
 
     def to_dict(self):
-        return {"filename": self.name,
-                "path": self.path,
-                "abstract": self.abstract
+        return {
+                    "filename": self.name,
+                    "path": self.path,
+                    "type": self.type,
+                    "abstract": self.abstract
                 }
 
     def summarize(self, function=None):
-        raise NotImplemented("Subclass should implement this method")
+        pass
+        ##raise NotImplemented("Subclass should implement this method")
