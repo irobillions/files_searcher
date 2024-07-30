76d84627f0469e1628fc4f20bae683f798c8dd11
irobilllions <christbouka14@yahoo.fr>
2024-07-30 02:05:29 -0400

    testing searchbyname

--- src/tests/test_search.py
+++ src/tests/test_search.py
@@ -1,39 +1,75 @@
-import unittest
-from unittest.mock import patch
+import sys
+import os
+import pytest
+from unittest.mock import patch, MagicMock
 
 from search.search_by_name import SearchByName
 
+@pytest.fixture
+def search_by_name():
+    directories = ['/mocked/dir']
+    filename = 'testfile'
+    return SearchByName(directories, filename)
 
-class TestSearchByName(unittest.TestCase):
+@patch('search.search_by_name.os.walk')
+def test_search_file_found(mock_os_walk, search_by_name):
+    # Mocking os.walk pour simuler le système de fichiers
+    mock_os_walk.return_value = [
+        ('/mocked/dir', ['subdir'], ['testfile.txt', 'otherfile.txt']),
+        ('/mocked/dir/subdir', [], ['anotherfile.txt'])
+    ]
 
-    def setUp(self):
-        self.directories = ['/mocked/dir']
-        self.filename = 'testfile'
-        self.searcher = SearchByName(self.directories, self.filename)
+    # Mocking FileInfos
+    with patch('search.search_by_name.FileInfos') as mock_file_infos:
+        mock_instance = mock_file_infos.return_value
+        mock_instance.to_dict.return_value = {
+            'name': 'testfile.txt',
+            'path': '/mocked/dir/testfile.txt',
+            'summary': ''
+        }
+        results = search_by_name.search()
 
-    @patch('src.search_by_name.os.walk')
-    def test_search_file_found(self, mock_os_walk):
-        # Mocking os.walk to simulate the file system
-        mock_os_walk.return_value = [
-            ('/mocked/dir', ['subdir'], ['testfile.txt', 'otherfile.txt']),
-            ('/mocked/dir/subdir', [], ['anotherfile.txt'])
+        # Vérification des résultats
+        assert len(results) == 1
+        assert results[0]['name'] == 'testfile.txt'
+        assert results[0]['path'] == '/mocked/dir/testfile.txt'
+
+
+
+@patch('search.search_by_name.os.walk')
+def test_search_file_not_found(mock_os_walk, search_by_name):
+    mock_os_walk.return_value = [
+        ('/mocked/dir', ['subdir'], ['otherfile.txt']),
+        ('/mocked/dir/subdir', [], ['anotherfile.txt'])
+    ]
+
+    results = search_by_name.search()
+    assert len(results) == 0
+
+
+@patch('search.search_by_name.os.walk')
+def test_search_directory_not_found(mock_os_walk, search_by_name):
+
+    mock_os_walk.return_value =  [
+            ('/mocked/dir', ['testfile'], ['otherfile.txt']),
+            ('/mocked/dir/testfile', [], ['anotherfile.txt'])
         ]
 
-        # Mocking FileInfos
-        with patch('src.search.files_infos.FileInfos') as mock_file_infos:
-            mock_instance = mock_file_infos.return_value
-            mock_instance.to_dict.return_value = {
-                'name': 'testfile.txt',
-                'path': '/mocked/dir/testfile.txt',
-                'summary': ''
-            }
-            results = self.searcher.search()
 
-            # Verifying the results
-            self.assertEqual(len(results), 1)
-            self.assertEqual(results[0]['name'], 'testfile.txt')
-            self.assertEqual(results[0]['path'], '/mocked/dir/testfile.txt')
+    with patch("search.search_by_name.FileInfos") as mock_file_infos:
+        mock_instance = mock_file_infos.return_value
+        mock_instance.to_dict.return_value = {
+            'name': 'testfile',
+            'path': '/mocked/dir/testfile',
+            'summary': ''
+        }
+
+        results = search_by_name.search()
+
+        assert len(results) == 1
+        assert results[0]['name'] == 'testfile'
+        assert results[0]['path'] == '/mocked/dir/testfile'
 
 
 if __name__ == '__main__':
-    unittest.main()
+    pytest.main()
