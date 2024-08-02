cebf8674749c62b6867c133992c5abe976df1388
irobilllions <christbouka14@yahoo.fr>
2024-08-02 04:24:09 -0400

    test for docx,doc,xlsx, xls, pdf

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import os
+import textract
 
-from search.search_by_type import SearchByType
+
+from search.search_by_type import SearchByTypeStrategy
 from utils.file_utils import *
 
 
@@ -14,8 +16,8 @@ class Main:
         only_file = get_boolean_input("Uniquement des fichiers ? (oui/non): ")
         pattern_to_search = get_filename()
 
-        self.searcher = SearchByType(pattern_to_search, directories)
-        results = self.searcher.search()
+        self.searcher = SearchByTypeStrategy()
+        results = self.searcher.search(directories, pattern_to_search)
 
         if only_file:
             results = [record for record in results if record["type"] != "Repertoire"]
@@ -24,7 +26,13 @@ class Main:
 
 
 if __name__ == "__main__":
-    app = Main()
-    res = app.run()
+    # app = Main()
+    # res = app.run()
+    #
+    # print(res)
 
-    print(res)
+    path = "D:\Ecole Polytechnique de Montreal\Sessions\A2022\MTH1101\Documents généraux\Plan-cours- A22.pdf"
+    keyword = "MacLaurin"
+    print(file_contain_keyword_pdf(path, keyword))
+
+
